41458ce14324676a897b518974120167defb40e6
Recep Çalışır <rpcalisir@gmail.com>
2023-02-22 20:34:38 +0300

    WPF project is created, Caliburn Micro package is added, Displaying the view when app is launched is set via Caliburn Micro, Resource dictionary is added into App.xaml and Bootstrapper is set as resource

---
+++ SMIV.DesktopUI/Bootstrapper.cs
@@ -0,0 +1,25 @@
+﻿using Caliburn.Micro;
+using SMIV.DesktopUI.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace SMIV.DesktopUI
+{
+    public class Bootstrapper : BootstrapperBase
+    {
+        public Bootstrapper()
+        {
+            Initialize();
+        }
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            //Determines the view to display when the application launches
+            DisplayRootViewForAsync<MainViewModel>();
+        }
+    }
+}
---
+++ SMIV.DesktopUI/ViewModels/MainViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SMIV.DesktopUI.ViewModels
+{
+    public class MainViewModel
+    {
+    }
+}
